3a541138207ae15291b052e6e3a145019a0e0060
Naveen Palaniswamy <naveen.palaniswamy@originenergy.com.au>
2017-08-12 19:39:10 +1000

    Adding additional parameters to constructor for contract initialization

--- contracts/NavCoin.sol
+++ contracts/NavCoin.sol
@@ -1,14 +1,27 @@
 pragma solidity ^0.4.15;
 
 contract NavCoin {
+
+    // contract level members
+    string tokenName;
+    string tokenSymbol;
+    unit8 decimalUnitsForDisplay;
     
     // add mapping for address to balance - should be public so that anyone can query balance
     // associated with an address
     mapping(address => uint256) public tokenBalance;
 
-    // constructor method that assigns initial supply value when contract is initialized
-    function NavCoin(unit256 initialSupply) {
+    // contract level events
+    event Transfer( address indexed from, address indexed to, unit256 value );
+
+    // constructor method that assigns the following values when contract is initialized
+    //initial supply value 
+    //token name
+    function NavCoin(string tokenName, string tokenSymbol, unit256 initialSupply, unit8 decimalUnitsForDisplay) {
+        this.tokenName = tokenName
+        this.tokenSymbol = tokenSymbol;
         tokenBalance[msg.sender] = initialSupply;
+        this.decimalUnitsForDisplay = decimalUnitsForDisplay;
     }
 
     // method to transfer tokens out of the contract to a recepient
@@ -17,15 +30,13 @@ contract NavCoin {
         //check if the contract has requested tokens in the first place
         //and make sure that there is no integer overflow issue with the recepient
 
-        if( _to == 0x0 ) throw;
-        if( tokenBalance[msg.sender] < _value )  throw;
-        if( tokenBalance[_to] + _value < tokenBalance[_to] ) throw;
+        if( _to == 0x0 || tokenBalance[msg.sender] < _value || tokenBalance[_to] + _value < tokenBalance[_to] ) throw;
 
         //deduct from origin and increment in destination
         tokenBalance[msg.sender] -= _value;
         tokenBalance[_to] += _value;
 
-        // fire transfer event
+        // fire transfer event to notify all nodes that a transfer event took place
         Transfer( msg.sender, _to, _value);
     }
 
