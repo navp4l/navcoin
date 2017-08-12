d590a9e8077a3536989bbeda7173084f888632a3
Naveen Palaniswamy <naveen.palaniswamy@originenergy.com.au>
2017-08-12 21:58:12 +1000

    remove commented lines

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-//var ConvertLib = artifacts.require("./ConvertLib.sol");
+var ConvertLib = artifacts.require("./ConvertLib.sol");
 var NavCoin = artifacts.require("./NavCoin.sol");
 
 module.exports = function(deployer) {
-  //deployer.deploy(ConvertLib);
-  //deployer.link(ConvertLib, NavCoin);
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, NavCoin);
   deployer.deploy(NavCoin);
 };
