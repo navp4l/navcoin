743a26fde457a452d6bff65368bfa4ff54fb379f
Naveen Palaniswamy <naveen.palaniswamy@originenergy.com.au>
2017-08-15 14:48:28 +1000

    Deploy contract with constructor args

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,5 +4,5 @@ var NavCoin = artifacts.require("./NavCoin.sol");
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, NavCoin);
-  deployer.deploy(NavCoin);
+  deployer.deploy(NavCoin, "NavCoin", "NCN", 100, 2);
 };
