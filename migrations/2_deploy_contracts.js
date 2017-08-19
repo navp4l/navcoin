1a0d538172c9ee6c9cf15c4cd35ca71b3977c59f
Naveen Palaniswamy <8531695+palanisn@users.noreply.github.com>
2017-08-20 07:39:52 +1000

    Update deployment scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,11 @@
 var SafeMathLib = artifacts.require("./SafeMathLib.sol");
+var ERC20TokenInterface = artifacts.require("./ERC20TokenInterface.sol");
+var Owned = artifacts.require("./Owned.sol");
 var NavCoinSimple = artifacts.require("./NavCoinSimple.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(ERC20TokenInterface);
+  deployer.deploy(Owned);
   deployer.deploy(SafeMathLib);
   deployer.link(SafeMathLib, NavCoinSimple);
   deployer.deploy(NavCoinSimple, "NavCoinSimple", "NCN", 100, 18);
